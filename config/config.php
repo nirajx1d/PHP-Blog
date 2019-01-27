68b4ea000eabc9602f72258c60940dbf33ae9427
Niraj Shubham <nirajshubham@gmail.com>
2019-01-27 23:51:42 +0530

    config file changed

--- config/config.php
+++ config/config.php
@@ -1,7 +1,7 @@
 <?php
 define('DB_HOST','localhost');
-define('DB_USER','root');
-define('DB_PASS','niraj@123');
+define('DB_USER','YOUR_USERNAME');
+define('DB_PASS','YOUR_PASSWORD');
 define('DB_NAME','blog');
 
 $site_description="Some random description...";
